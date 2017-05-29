88b2951d215cb3fa8eefabf4e74a1fffd652add1
Tobias Oetzel <tobias.oetzel@sap.com>
2017-05-29 15:02:04 +0200

    step nr. 17

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -22,12 +22,16 @@ sap.ui.define([
 			// create dialog lazily
 			if (!oDialog) {
 				// create dialog via fragment factory
-				oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.wt.view.HelloDialog");
+				oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.wt.view.HelloDialog", this);
 				// connect dialog to view (models, lifecycle)
 				oView.addDependent(oDialog);
 			}
 
 			oDialog.open();
+		},
+
+		onCloseDialog : function () {
+			this.getView().byId("helloDialog").close();
 		}
 	});
 
