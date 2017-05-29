3601649f8e4348494a63584ca6354d84029e156c
Tobias Oetzel <tobias.oetzel@sap.com>
2017-05-29 15:01:44 +0200

    step nr. 36

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/controller/HelloDialog"
-], function (UIComponent, JSONModel, HelloDialog) {
+	"sap/ui/demo/wt/controller/HelloDialog",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
@@ -24,6 +25,11 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
+
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
 
 			// set dialog
 			this._helloDialog = new HelloDialog(this.getRootControl());
